3a17bd99d55b7c565dfe75be33d8342028dff1ac
Adam Hanna <ahanna@alumni.mines.edu>
2020-04-06 16:28:15 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Highcharts from 'highcharts/highstock';
+//import Highcharts from 'highcharts'; <<<<< this import doesn't work, either
 
-function App() {
+const App = () => {
+  const [data, setData] = useState<Array<Array<number>>>([]);
+
+  useEffect(() => {
+    ;(async function() {
+      try {
+        const tmpData = Highcharts.getJSON('https://www.highcharts.com/samples/data/aapl-c.json');
+        setData(tmpData);
+      } catch(e) {
+        window.alert(e);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log('data', data);
+    if (!data || data.length === 0) return;
+
+    Highcharts.stockChart('container', {
+      rangeSelector: {
+        selected: 1
+      },
+
+      title: {
+        text: 'AAPL Stock Price'
+      },
+
+      series: [{
+        name: 'AAPL',
+        data: data,
+        tooltip: {
+          valueDecimals: 2
+        }
+      }]
+    });
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container" style={{ width: '100%', height: '100%' }} />
     </div>
   );
 }
